2f0d3691ac26c30353e168d0c746a407509f9aae
DanziNN <danielguimaraessouza8@gmail.com>
2023-08-10 23:54:18 -0300

    Olá

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,21 +9,27 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.greenAccent,
       body: Padding(
-        padding: const EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(55.0),
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: 40,
+            children:[
+              Container(
+                width: 150,
+                height: 200,
+                child: Image.asset("assets/logo.png"),
+              ),
               
+              SizedBox(
+                height: 70,
+                width: 5,
                 child:TextField(
                   autofocus: true,
                   keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(color: Colors.blue, fontSize: 30),
+                  style: TextStyle(color: Colors.blue, fontSize: 23),
                   decoration: InputDecoration(
                     labelText: "E-mail do usuário",
                     labelStyle: TextStyle(color: Colors.black),
@@ -31,32 +37,37 @@ class _loginState extends State<login> {
               ),
               ),
               SizedBox(
-                width: 40,
+                height: 70,
+                width: 5,
                 
                 child: TextField(
                   obscureText: true,
                   autofocus: true,
                   keyboardType: TextInputType.text,
-                  style: TextStyle(color: Colors.blue, fontSize: 30 ),
+                  style: TextStyle(color: Colors.blue, fontSize: 23 ),
                   decoration: InputDecoration(
                     labelText: "Senha do usuário",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 30),
+                    labelStyle: TextStyle(color: Colors.black, fontSize: 23),
                 )
               ),
               ),
               Container(
                 margin: EdgeInsets.only(top: 30, bottom: 30),
               ),
-              TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.blue,elevation: 10),
-                onPressed: (){print("Deu certo?");
-              }, 
-              child: Text(
-                "Entrar",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white),
-                ))
-            ],
+              ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: ElevatedButton(onPressed: (){
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(10.0),
+                  backgroundColor: Colors.red, 
+                ),
+                child: Text(
+                  "Entrar",
+                  style: TextStyle(color: Colors.black),
+                ),
+              ))
+            ]
           ),
         ),
       ),
