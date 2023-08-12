0f4efd26d448512ada475dc43850b5a11d5848fb
DanziNN <danielguimaraessouza8@gmail.com>
2023-08-12 00:43:33 -0300

    agora sim

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,74 +2,82 @@ import 'package:flutter/material.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
+
   @override
   State<login> createState() => _loginState();
 }
+
 class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.greenAccent,
       body: Padding(
-        padding: const EdgeInsets.all(55.0),
+        padding: EdgeInsets.all(15),
         child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
-            children:[
+            children: <Widget>[
               Container(
+                child: Image.asset("assets/logo.png"),
                 width: 150,
                 height: 200,
-                child: Image.asset("assets/logo.png"),
-              ),
-              
-              SizedBox(
-                height: 70,
-                width: 5,
-                child:TextField(
-                  autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  style: TextStyle(color: Colors.blue, fontSize: 23),
-                  decoration: InputDecoration(
-                    labelText: "E-mail do usuário",
-                    labelStyle: TextStyle(color: Colors.black),
-                )
-              ),
               ),
               SizedBox(
-                height: 70,
-                width: 5,
-                
-                child: TextField(
-                  obscureText: true,
-                  autofocus: true,
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(color: Colors.blue, fontSize: 23 ),
-                  decoration: InputDecoration(
-                    labelText: "Senha do usuário",
-                    labelStyle: TextStyle(color: Colors.black, fontSize: 23),
-                )
+                width: 400,
+                height: 60,
+                child: TextFormField(
+              autofocus: true,
+              keyboardType: TextInputType.emailAddress,
+              style: TextStyle(color: Colors.black,fontSize: 20),
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(color: Colors.black),
               ),
+            ), 
               ),
-              Container(
-                margin: EdgeInsets.only(top: 30, bottom: 30),
+              SizedBox(
+                width: 400,
+                height: 60,
+                child: TextFormField(
+              autofocus: true,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              style: TextStyle(color: Colors.black,fontSize: 20),
+              decoration: InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(color: Colors.black,fontSize: 20),
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50.0),
-                child: ElevatedButton(onPressed: (){
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.all(10.0),
-                  backgroundColor: Colors.red, 
-                ),
-                child: Text(
-                  "Entrar",
-                  style: TextStyle(color: Colors.black),
-                ),
-              ))
-            ]
-          ),
+            ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+            SizedBox(
+              height: 60,
+              width: 70,
+              child:  ElevatedButton(
+              onPressed: (){
+              print("Conseguimos!");
+            },
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(8),
+              backgroundColor: Colors.green,
+            ),
+            child: Text(
+              "Entrar",
+              style: TextStyle(color: Colors.black,fontSize: 20),
+              ),
+            ), 
+            ),
+            SizedBox(
+              height: 50,
+              width: 70,
+            )
+        ]
         ),
+      ),
       ),
     );
   }
